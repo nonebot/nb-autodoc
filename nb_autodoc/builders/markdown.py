1959e6dd85b4111c4e2c93de762dbc12b243793c
iyume <iyumelive@gmail.com>
2022-01-19 14:06:15 +0800

    fix: ^^change

--- nb_autodoc/builders/markdown.py
+++ nb_autodoc/builders/markdown.py
@@ -75,7 +75,7 @@ class MarkdownBuilder(Builder):
                 current_filepath.pop(0)
             for _ in range(len(current_filepath) - 1):
                 relatived_path.append("..")
-            if not relatived_path[0] == "..":
+            if relatived_path and not relatived_path[0] == "..":
                 relatived_path.append(".")
             relatived_path.extend(filepath)
         return "[{}]({}#{})".format(
