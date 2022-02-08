270ea14706a5fff60f3fd5bcdff11d437a5ca612
iyume <iyumelive@gmail.com>
2022-02-08 15:41:29 +0800

    feat: add vuepress as cli flag

--- nb_autodoc/__main__.py
+++ nb_autodoc/__main__.py
@@ -2,14 +2,20 @@ import click
 
 from nb_autodoc import Module
 from nb_autodoc.builders.markdown import MarkdownBuilder
+from nb_autodoc.builders import helpers
 
 
 @click.command()
+@click.option("--vuepress", default=False, is_flag=True)
 @click.option("-o", "--output-dir", default="build")
 @click.argument("module_name")
-def cli_main(module_name, output_dir):
+def cli_main(module_name, output_dir, vuepress):
     module = Module(module_name)
-    builder = MarkdownBuilder(module, output_dir=output_dir)
+    builder = MarkdownBuilder(
+        module,
+        output_dir=output_dir,
+        slugify=helpers.vuepress_slugify if vuepress else None,
+    )
     builder.write()
 
 
