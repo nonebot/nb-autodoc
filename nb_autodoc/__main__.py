6b9632fd65e9245d30bcc26a7e0bc10d97164b97
iyume <iyumelive@gmail.com>
2022-08-07 22:16:33 +0800

    add basic console script

--- nb_autodoc/__main__.py
+++ nb_autodoc/__main__.py
@@ -1,1 +1,12 @@
 import click
+
+
+@click.command()
+@click.argument("module_name")
+@click.option("-o", "--output-dir", default="build")
+def main(module_name: str, output_dir: str) -> None:
+    ...
+
+
+if __name__ == "__main__":
+    main()
