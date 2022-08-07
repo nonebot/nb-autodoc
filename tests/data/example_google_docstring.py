6208eb2e36023b3395088d0b4618dbae7245cb03
iyume <iyumelive@gmail.com>
2022-08-07 20:08:30 +0800

    fix lint

--- tests/data/example_google_docstring.py
+++ tests/data/example_google_docstring.py
@@ -75,6 +75,7 @@ def function_with_pep484_type_annotations(param1: int, param2: str) -> bool:
         The return value. True for success, False otherwise.
 
     """
+    ...  # fix lint
 
 
 def module_level_function(param1, param2=None, *args, **kwargs):
