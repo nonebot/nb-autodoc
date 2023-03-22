nonebot/nb-autodoc

commit 751ebd4cf39078a269992237cf49cd52431981b3

fix: annotation text is not escaped

nb_autodoc/builders/markdown.py
@@ -39,6 +39,19 @@ _descr_role_re = re.compile(
 )
 
 
+def escape_md_chars(s: str) -> str:
+    return (
+        s.replace("*", r"\*")
+        .replace("#", r"\#")
+        .replace("(", r"\(")
+        .replace(")", r"\)")
+        .replace("<", r"\<")
+        .replace(">", r"\>")
+        .replace("_", r"\_")
+        .replace("`", r"\`")
+    )
+
+
 # renderer utils
 def _find_kind_from_roles(roles: List[nodes.Role]) -> Optional[str]:
     for role in roles:
@@ -249,7 +262,7 @@ class Renderer:
             if warn_notfound:
                 logger.warning(warn_notfound)
             return dobj.qualname
-        text = text or dobj.qualname
+        text = escape_md_chars(text or dobj.qualname)
         if modulename == self.current_module.name:
             return f"[{text}](#{anchor})"
         path = self.builder.paths[modulename]
@@ -316,9 +329,11 @@ class Renderer:
                 if doc_arg and doc_arg.annotation:
                     annotation = bind_module.build_static_ann(
                         ast.parse(doc_arg.annotation, mode="eval").body
-                    ).get_doc_linkify(self.add_link)
+                    ).get_doc_linkify(self.add_link, escape_md_chars)
                 elif p.annotation is not Parameter.empty:
-                    annotation = p.annotation.get_doc_linkify(self.add_link)
+                    annotation = p.annotation.get_doc_linkify(
+                        self.add_link, escape_md_chars
+                    )
                 arg = nodes.ColonArg(p.name, annotation, [], "", "")
                 if doc_arg:
                     arg.descr = doc_arg.descr
@@ -345,7 +360,7 @@ class Renderer:
                 if doc_arg.annotation:
                     annotation = bind_module.build_static_ann(
                         ast.parse(doc_arg.annotation, mode="eval").body
-                    ).get_doc_linkify(self.add_link)
+                    ).get_doc_linkify(self.add_link, escape_md_chars)
                 new_args.kwonlyargs.append(
                     nodes.ColonArg(
                         name, annotation, [], doc_arg.descr, doc_arg.long_descr
@@ -370,7 +385,7 @@ class Renderer:
                 assert rets.value.annotation, "Returns ColonArg annotation must be str"
                 annotation = bind_module.build_static_ann(
                     ast.parse(rets.value.annotation, mode="eval").body
-                ).get_doc_linkify(self.add_link)
+                ).get_doc_linkify(self.add_link, escape_md_chars)
                 descr = rets.value.descr
                 long_descr = rets.value.long_descr
             else:
@@ -380,7 +395,9 @@ class Renderer:
             new_rets.value.descr = descr
             new_rets.value.long_descr = long_descr
         if annotation is None and sig and sig.return_annotation is not Parameter.empty:
-            annotation = sig.return_annotation.get_doc_linkify(self.add_link)
+            annotation = sig.return_annotation.get_doc_linkify(
+                self.add_link, escape_md_chars
+            )
         elif annotation is None:
             annotation = "untyped"
         new_rets.value.annotation = annotation
@@ -466,11 +483,11 @@ class Renderer:
         self.newline("- **类型:**")
         if dobj.annotation:
             self.write(" ")
-            self.write(dobj.annotation.get_doc_linkify(self.add_link))
+            self.write(dobj.annotation.get_doc_linkify(self.add_link, escape_md_chars))
         elif dobj.doctree and dobj.doctree.annotation:
             dobj.module.build_static_ann(
                 ast.parse(dobj.doctree.annotation, mode="eval").body
-            ).get_doc_linkify(self.add_link)
+            ).get_doc_linkify(self.add_link, escape_md_chars)
         inlinevalue = {}
         if dobj.doctree:
             inlinevalue = _extract_inlinevalue(dobj.doctree)
@@ -575,7 +592,7 @@ class Renderer:
         if link_ann and dsobj.annotation:
             dsobj.annotation = self.current_module.build_static_ann(
                 ast.parse(dsobj.annotation, mode="eval").body
-            ).get_doc_linkify(self.add_link)
+            ).get_doc_linkify(self.add_link, escape_md_chars)
         self.fill("- ")
         if dsobj.name:
             with self.delimit("`", "`"):

nb_autodoc/annotation.py
@@ -298,10 +298,13 @@ class AnnExprVisitor:
         globalns: dict[str, T_Definition] = frozendict(),
         add_link: t.Callable[[T_Definition], str] = _add_link_empty,
         eval_refname: t.Callable[[str], t.Optional[T_Definition]] = lambda x: None,
+        escape_impl: t.Callable[[str], str] = lambda x: x,
     ) -> None:
         self.globalns = globalns
         self.add_link = add_link
         self.eval_refname = eval_refname
+        # escape for name contains md chars; or Literal string
+        self.escape_impl = escape_impl
         self._builder: list[str] = []
 
     def write(self, s: str) -> None:
@@ -342,7 +345,7 @@ class AnnExprVisitor:
         if dobj:
             self.write(self.add_link(dobj))
         else:
-            self.write(name)
+            self.write(self.escape_impl(name))
 
     def visit_TypingName(self, annexpr: TypingName) -> None:
         if annexpr.tp_name in _py310_ga_tpname:
@@ -359,7 +362,7 @@ class AnnExprVisitor:
                 lambda: self.write(", "),
                 lambda arg: self.visit_Name(arg)
                 if isinstance(arg, Name)
-                else self.write(repr(arg)),
+                else self.write(self.escape_impl(repr(arg))),
                 annexpr.args,
             )
 
@@ -431,13 +434,18 @@ class Annotation:
             return _ga_subst_outer_check(self.ann, "ClassVar")
         return False
 
-    def get_doc_linkify(self, add_link: t.Callable[[T_Definition], str]) -> str:
+    def get_doc_linkify(
+        self,
+        add_link: t.Callable[[T_Definition], str],
+        escape_impl: t.Callable[[str], str],
+    ) -> str:
         return AnnExprVisitor(
             globalns=self.globalns,
             add_link=add_link,
             eval_refname=self.manager.get_definition_dotted
             if self.manager
             else lambda x: None,
+            escape_impl=escape_impl,
         ).render(self.ann)
 
     def __str__(self) -> str:

commit 2da733fc316edaa2ab0ef7a05d093292f454d3da

fix: incorrect impl caused by 751ebd4

nb_autodoc/builders/markdown.py
@@ -255,14 +255,21 @@ class Renderer:
             self.write(f" {{#{heading_id_slugify_impl(dobj)}}}")
 
     def add_link(
-        self, dobj: T_Definition, *, warn_notfound: str = "", text: Optional[str] = None
+        self,
+        dobj: T_Definition,
+        *,
+        warn_notfound: str = "",
+        text: Optional[str] = None,
+        autoescape: bool = True,
     ) -> str:
         modulename, anchor = self.builder.get_anchor_ref(dobj)
         if not anchor:
             if warn_notfound:
                 logger.warning(warn_notfound)
             return dobj.qualname
-        text = escape_md_chars(text or dobj.qualname)
+        text = text or dobj.qualname
+        if autoescape:
+            text = escape_md_chars(text)
         if modulename == self.current_module.name:
             return f"[{text}](#{anchor})"
         path = self.builder.paths[modulename]
@@ -289,6 +296,7 @@ class Renderer:
                     warn_notfound=f"ref {matchtext!r} found {dobj.fullname!r} "
                     "which is unlinkable",
                     text=text,
+                    autoescape=False,
                 )
             else:
                 logger.warning(f"ref {matchtext!r} is not a member")