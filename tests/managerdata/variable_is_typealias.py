aeb2ee1d880e3a09d0a7fd708c7488bf2dd88b16
呵呵です <51957264+shoucandanghehe@users.noreply.github.com>
2026-03-15 17:58:31 +0800

    fix: multiline type alias parsing (#2)
    https://github.com/nonebot/nonebot2/actions/runs/23106884206/job/67117564595?pr=3898

--- tests/managerdata/variable_is_typealias.py
+++ tests/managerdata/variable_is_typealias.py
@@ -8,6 +8,17 @@ b: te.TypeAlias = "dict[str, str]"
 # TODO: add this after ast unparse expr implemented
 c: "te.TypeAlias" = "dict[str, str]"
 
+f: te.TypeAlias = (
+    t.Union[
+        t.Set[int],
+        t.Set[str],
+        t.Dict[int, "f"],
+        t.Dict[str, "f"],
+        None,
+    ]
+)
+
+
 d: int = 1
 
 e = 1
